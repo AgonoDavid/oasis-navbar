9b1d6929f568791366dc29be69df812bbb0141ee
AgonoDavid <davidagono239@gmail.com>
2023-04-22 16:14:52 +0100

    new

---
+++ oasis-nav/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <nav>
+      <div className="nav_left">
+        <img
+          src="./images/IMG-20230314-WA0010 25.png
+        "
+          alt=""
+        />
+        <p>INITIATIVE</p>
+      </div>
+      <div className="nav_list">
+        <a href="'">Home</a>
+
+        <a href="'">About Us</a>
+
+        <a href="'">Contact Us</a>
+
+        <a href="'">Support</a>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
