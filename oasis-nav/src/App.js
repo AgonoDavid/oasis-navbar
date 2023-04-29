2daa42affb77ec81a45430c0826096b2643008a1
AgonoDavid <davidagono239@gmail.com>
2023-04-29 01:04:45 +0100

    new

--- oasis-nav/src/App.js
+++ oasis-nav/src/App.js
@@ -1,28 +1,7 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./navbar";
 
 function App() {
-  return (
-    <nav>
-      <div className="nav_left">
-        <img
-          src="./images/IMG-20230314-WA0010 25.png
-        "
-          alt=""
-        />
-        <p>INITIATIVE</p>
-      </div>
-      <div className="nav_list">
-        <a href="'">Home</a>
-
-        <a href="'">About Us</a>
-
-        <a href="'">Contact Us</a>
-
-        <a href="'">Support</a>
-      </div>
-    </nav>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ oasis-nav/src/navbar.js
@@ -0,0 +1,44 @@
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./images/logomain.png";
+import { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { menu } from "react-icons-kit/feather/menu";
+import { x } from "react-icons-kit/feather/x";
+
+const Navbar = () => {
+  const logotext = "INITIATIVE";
+
+  const [toggle, setToggle] = useState(false);
+
+  const handletoggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <header>
+      <nav className={toggle ? "navbar expanded" : "navbar"}>
+        <div className="nav_left">
+          <img src={logo} alt="logo" className="img-logo" />
+          <p>{logotext}</p>
+        </div>
+        <ul className="links">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Support</li>
+          <Button className="btn-donate">Donate</Button>
+        </ul>
+        <div className="toggle-icon" onClick={handletoggle}>
+          {toggle ? (
+            <Icon icon={x} size={28} />
+          ) : (
+            <Icon icon={menu} size={28} />
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
